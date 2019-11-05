0a63cd0fa620f80722d95d3c8b5add11e6ba3ae0
Anthony <anthonymarques@outlook.com>
2019-11-05 20:35:29 +0200

    spot the difference

---
+++ models/cart.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'cart.json'
+);
+
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      // Analyze the cart => Find existing product
+      const exisitingProductIndex = cart.products.findIndex(
+        prod => (prod.id === id)
+      );
+      const exisitingProduct = cart.products[exisitingProductIndex];
+      let updatedProduct;
+      if (exisitingProduct) {
+        updatedProduct = { ...exisitingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[exisitingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice = cart.totalPrice + +productPrice;
+      fs.writeFile(p, JSON.stringify(cart), err => {
+        console.log(err);
+      });
+    });
+  }
+};
